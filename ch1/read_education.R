127626ad07d3cdd597f479e1f1c670a7302f69ec
njtierney <nicholas.tierney@gmail.com>
2026-08-31 09:52:39 +1000

    final tweaks

--- ch1/read_education.R
+++ ch1/read_education.R
@@ -3,13 +3,20 @@ education_path <- function(year) {
   glue("data/tidy/education_{year}.csv") |> here()
 }
 
-# Read any number of years, and put the year back on as a column.
+# Undo that naming: pull the year back out of a path, as text.
 # basename() first, or parse_number() finds a number in the folder names.
+year_from_path <- function(path) {
+  basename(path) |>
+    parse_number() |>
+    as.character()
+}
+
+# Read any number of years, and put the year back on as a column.
 read_education <- function(year) {
   files <- education_path(year)
   education_raw <- read_csv(files, id = "path")
 
   education_raw |>
-    mutate(year = basename(path) |> parse_number() |> as.character()) |>
+    mutate(year = year_from_path(path)) |>
     select(-path)
 }
