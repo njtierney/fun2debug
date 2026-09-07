85d84c447ea53d3db25b9345fe96069c75ad429e
njtierney <nicholas.tierney@gmail.com>
2026-09-07 10:33:43 +1000

    reframe looking insidfe

---
+++ R/read_education2.R
@@ -0,0 +1,14 @@
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#'
+#' @title
+#' @param years
+#' @return
+#' @author njtierney
+#' @export
+read_education2 <- function(years = 2014:2023) {
+
+  NULL
+
+}
